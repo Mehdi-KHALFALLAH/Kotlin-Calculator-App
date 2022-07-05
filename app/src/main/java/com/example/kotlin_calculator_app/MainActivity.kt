54f6ab7f0af9fb106d496736293130691ce09b04
Mehdi Khalfallah <contact.mehdi.khalfallah@gmail.com>
2022-07-05 13:33:12 +0100

    implementing the dot and CLR

--- app/src/main/java/com/example/kotlin_calculator_app/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_calculator_app/MainActivity.kt
@@ -3,14 +3,29 @@ package com.example.kotlin_calculator_app
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private var tvInput:TextView?=null
+    var lastNumeric: Boolean = false
+    var lastDot: Boolean = false
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tvInput = findViewById(R.id.tvInput)
     }
+
+
     fun onDigit (view: View) {
-        Toast.makeText(this, "button clicked", Toast.LENGTH_LONG).show()
+        tvInput?.append((view as Button).text)
+    }
+
+    fun onCLear (view: View) {
+        tvInput?.text = ""
     }
 }
